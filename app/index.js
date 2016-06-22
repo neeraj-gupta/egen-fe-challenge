34606df8515c5e964cca8bde24df71417717ac60
neeraj-gupta <neeraj.gupta@sjsu.edu>
2016-06-21 22:39:24 -0700

    update structure

--- app/index.js
+++ app/index.js
@@ -1,45 +1,45 @@
 /**
  * Created by Neeraj on 6/21/2016.
  */
+(function(){
+    'use strict';
 
-'use strict';
+    angular
+        .module('myApp')
+        .config(configure);
 
-angular
-    .module('myApp', ['ui.router'])
-    .config(configure);
+    configure.$inject = ['$stateProvider', '$urlRouterProvider'];
 
-configure.$inject = ['$stateProvider', '$urlRouterProvider',]
+    function configure($stateProvider, $urlRouterProvider){
 
-function configure($stateProvider, $urlRouterProvider){
+        $stateProvider.state('user-list', {
+            url : '/user-list',
+            templateUrl : 'views/partial-user-list.html',
+            controller : 'userController',
+            controllerAs : 'userVm'
+        });
 
-    $stateProvider.state('userList', {
-        url : '/user-list',
-        templateUrl : 'partial-partial-user-list.html',
-        controller : 'userController',
-        controllerAs : 'userVm'
-    });
+        $stateProvider.state('user', {
+            url : '/user',
+            templateUrl : 'views/partial-user.html',
+            controller : 'userController',
+            controllerAs : 'userVm'
+        });
 
-    $stateProvider.state('user', {
-        url : '/user',
-        templateUrl : 'partial-user.html',
-        controller : 'userController',
-        controllerAs : 'userVm'
-    });
+        $stateProvider.state('create-user', {
+            url : '/create',
+            templateUrl : 'views/partial-create-user.html',
+            controller : 'userController',
+            controllerAs : 'userVm'
+        });
 
-    $stateProvider.state('create-user', {
-        url : '/create',
-        templateUrl : 'partial-create-user.html',
-        controller : 'userController',
-        controllerAs : 'userVm'
-    });
+        $stateProvider.state('menu-drawer', {
+            url : '/menu',
+            templateUrl : 'views/partial-menu-drawer.html',
+            controller : 'menuController',
+            controllerAs : 'menuVm'
+        });
 
-    $stateProvider.state('menu-drawer', {
-        url : '/menu',
-        templateUrl : 'partial-menu-drawer.html',
-        controller : 'menuController',
-        controllerAs : 'menuVm'
-    });
-
-    $urlRouterProvider.otherwise('user');
-}
-
+        $urlRouterProvider.otherwise('user-list');
+    }
+})();
