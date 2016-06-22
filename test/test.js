6ace7fb1a9d35da144bb0797fabd784efc4333fd
neeraj-gupta <neeraj.gupta@sjsu.edu>
2016-06-22 16:58:57 -0700

    Adding test frame work dependencies

--- test/test.js
+++ test/test.js
@@ -1,19 +1,29 @@
 /**
  * Created by Neeraj on 6/22/2016.
  */
-describe('Person', function () {
+describe('userListController', function() {
+    var scope, controller;
 
-    var Person;
-    beforeEach(module('myTestApp'));
-    beforeEach(inject(function (_Person_) {
-        Person = _Person_;
+    beforeEach(inject(function($controller, $rootScope) {
+        scope = $rootScope.$new();
+        controller = $controller('MyCtrl', {
+            $scope: scope
+        });
     }));
 
-    describe('Constructor', function () {
-        it('assigns a name', function () {
-            expect(new get('Ben')).to.have.property('name', 'Ben');
-        });
-
+    it('has correct initial values', function() {
+        expect(scope.value).toBe(0);
+        expect(scope.maxValue).toBe(3);
     });
 
+    it('increments correctly', function() {
+        scope.incrementValue();
+        expect(scope.value).toBe(1);
+        scope.incrementValue();
+        expect(scope.value).toBe(2);
+        scope.incrementValue();
+        expect(scope.value).toBe(3);
+        scope.incrementValue();
+        expect(scope.value).toBe(0);
+    });
 });
